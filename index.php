cc5784ca6643aa6ebe179656f60825db3a90b88e
Ikon-design <77398364+Ikon-design@users.noreply.github.com>
2021-03-26 17:18:28 +0100

    Mise en place de l'archi MVC

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+ define("ROOT", str_replace('index.php','',$_SERVER['SCRIPT_FILENAME']));
+ include(ROOT.'app/Controller.php');
+ include(ROOT.'app/Model.php');
+
+ //Sépare les parametre de l'url a partir des '/'
+ $params = explode('/', $_GET['p']);
+
+ if($params[0] != ""){
+     // Met un maj sur la premiere lettre du premier parametre
+     $controller = ucfirst($params[0]);
+     // Test si un deuxieme parametre existe sinon renvoie index
+     $action = isset($params[1]) ? $params[1] : 'index';
+     // Import le fichier php appelé avec l'url
+     include (ROOT.'controllers/'.$controller.'.php');
+
+     $controller = new $controller();
+     $controller->$action();
+     if (method_exists($controller, $action)){
+         $controller->action();
+     }else{
+         http_response_code(404);
+         echo "La page demandée n'existe pas";
+     }
+ }else{
+    include (ROOT.'controllers/Main.php');
+    $controller = new Main();
+    $controller->index();
+ }
+
+ // indexView();
